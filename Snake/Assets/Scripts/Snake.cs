1b9c8e8324337270c6bf13eb1a4cb3a03b28ac12
cristina1997 <cristinaflorinaeu@gmail.com>
2018-03-27 23:24:57 +0100

    Snake Movement

---
+++ Snake/Assets/Scripts/Snake.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour {
+
+    private Snake next;
+
+    public void setNext(Snake inside)
+    {
+        next = inside;
+    }
+
+    public Snake getNext()
+    {
+        return next;
+    }
+
+    public void RemoveTail()
+    {
+        Destroy(this.gameObject);
+    }
+}

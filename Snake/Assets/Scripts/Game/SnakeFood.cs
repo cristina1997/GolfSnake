736d02f6b010745f4896bb53dd9abcca3eb13ccb
cristina1997 <cristinaflorinaeu@gmail.com>
2018-04-02 19:50:05 +0100

    Separate Scripts for Food & Movement

--- Snake/Assets/Scripts/Game/SnakeFood.cs
+++ Snake/Assets/Scripts/Game/SnakeFood.cs
@@ -2,18 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
+// Code adapted from https://www.youtube.com/watch?v=Sau81jWbGRY&index=5&list=PLWeGoBm1YHVhc51TYY7fTLNbA02qkyLrA&ab_channel=InfoGamer
 public class SnakeFood : MonoBehaviour {
-    GameObject foodCount;
+    public int xSpawn, ySpawn;
+    public static int foodEaten;
+    public int score;
+    public GameObject food, currentFood;
+    public Text scoreText;
 
+    private void OnEnable()
+    {
+        Snake.eat += Eat;    
+    }
+
+    private void OnDisable()
+    {
+        Snake.eat -= Eat;
+    }
 
     private void Start()
     {
-        
+        CreateFood();
+        foodEaten = 0;
     }
 
-    private void Update()
+    void CreateFood()
     {
-        
+        // Vector 2 position to instantiate the food at
+        int xTemp = Random.Range(-xSpawn, xSpawn);
+        int yTemp = Random.Range(-ySpawn, ySpawn);
+
+        // Instantiate food object
+        currentFood = (GameObject)Instantiate(food, new Vector2(xTemp, yTemp), Quaternion.Euler(new Vector3(90, 0, 0)));
+        StartCoroutine(CheckRenderer(currentFood));
+
+    } // CreateFood
+
+    IEnumerator CheckRenderer(GameObject inside)
+    {
+        // Check if food is visible
+        // If food spawned within camera view it's set to false and is set to true at the end of the first frame instantiated
+        // If spawned off view of our camera it will always be false
+        yield return new WaitForEndOfFrame();                                                           // Waits until the end of the current frame and then execute code after it
+
+        // Check to see if game object passed into this function is visible
+        if (inside.GetComponent<Renderer>().isVisible == false)
+        {
+            // Make sure we have food object
+            if (inside.tag == "Food")
+            {
+                Destroy(inside);
+                CreateFood();
+            }
+        }
+    } // CheckRenderer
+
+    void Eat(string eatenObject)
+    {
+        // Make sure eatenObejct received from Snake is the food
+        if (eatenObject == "Food")
+        {
+            // Instantiate new food Object
+            CreateFood();
+            foodEaten++;
+            score += 10;
+            scoreText.text = score.ToString();
+            int temp = PlayerPrefs.GetInt("HighScore");
+
+            if (score > temp)
+            {
+                PlayerPrefs.SetInt("HighScore", 0);
+            }
+        }
+
+        if (eatenObject == "Snake")
+        {
+            // Prevents snake from moving any further
+            CancelInvoke("Timer");
+            GameOver();
+        }
+
+    } // Eat
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene(2);
     }
 }

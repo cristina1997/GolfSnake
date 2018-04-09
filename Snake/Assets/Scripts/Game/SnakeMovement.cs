eda7ae925d89fbc3e68474688293636fdc94e36f
cristina1997 <cristinaflorinaeu@gmail.com>
2018-04-09 13:38:41 +0100

    Delete Mobile Access example

--- Snake/Assets/Scripts/Game/SnakeMovement.cs
+++ Snake/Assets/Scripts/Game/SnakeMovement.cs
@@ -22,7 +22,7 @@ public class SnakeMovement : MonoBehaviour
     public const float speedTimerInit = 0.05f, speedResetInit = 300f, startSpeedValue = 0.5f;
     public float speedTimerCountdown, speedResetCountdown;
    // private Vector2 touchOrigin = -Vector2.one;                                 
-    // public int horizontal, vertical;
+
     private void Start()
     {
         speedDisplay = GameObject.Find("SpeedText").GetComponent<Text>();
@@ -184,77 +184,7 @@ public class SnakeMovement : MonoBehaviour
 
     } // ChangeDirectionMobile
 
-    /*public void ChangeDirectionMob()
-    {
-        horizontal = 0;
-        vertical = 0;
-
-        // Check if we are running on Windows Phone 10
-        #if UNITY_WEBPLAYERUNITY_WIN10
-
-            // Check if Input has registered more than zero touches
-            if (Input.touchCount > 0)
-            {
-
-                // Store the first touch detected
-                Touch myTouch = Input.touches[0];
-                
-                // Check if the phase of that touch Began
-                if (myTouch.phase == TouchPhase.Began)
-                {
-                    // If so, set touchOrigin to the position of that touch
-                    touchOrigin = myTouch.position;
-                } // If the touch phase did not begin but Ended instead and the x of touchOrigin is greater or equal to zero:
-                else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
-                {
-                    
-                    // If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
-                    Vector2 = touchEnd = myTouch.position;
-
-                    // Calculate the difference between the beginning and end of the touch on the x axis.
-                    float x = touchEnd.x - touchOrigin.x;
-
-                    // Calculate the difference between the beginning and end of the touch on the y axis.            
-                    float y = touchEnd.y - touchOrigin.y;
-
-                    // Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
-                    touchOrigin.x = -1;
-
-                    // Check if the difference along the x axis is greater than the difference along the y axis.        
-                    if (Mathf.Abs(x) > Mathf.Abs(y))
-                        // If x is greater than zero, set horizontal to 1
-                        // Otherwise set it to -1    
-                        horizontal = x > 0 ? 1 : -1;
-                    else
-                        // If y is greater than zero, set horizontal to 1
-                        // Otherwise set it to -1
-                        vertical = y > 0 ? 1 : -1;
-                } // if...else if
-            } // if
-    #endif
-        if (horizontal != 0 || vertical != 0)
-        {
-            if (vertical > 0)
-            {
-                direction = "UP";
-            }
-            else if (vertical < 0)
-            {
-                direction = "DOWN";
-            }
-            else if (horizontal > 0)
-            {
-                direction = "RIGHT";
-            }
-            else if (horizontal < 0)
-            {
-                direction = "LEFT";
-            } // if...else if
-        } // if
-
-    } // ChangeDirectionMob
-    */
-
+ 
     void Wrap()
     {
         switch (direction)
